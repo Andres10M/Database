53d1c1cef89c18fd77d013f55a65d96abda93303
Andres10M <pasari@sudamericano.edu.ec>
2024-10-16 16:21:50 -0500

    primer commit

---
+++ src/main/kotlin/com/example/demo/controller/HomeController.kt
@@ -0,0 +1,29 @@
+package com.example.demo.controller
+
+import com.example.demo.model.Model
+import jakarta.websocket.server.PathParam
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/home")
+class HomeController {
+
+    /*@GetMapping("/{id}")
+    fun home(@PathVariable("id") id: Int ): String {
+        return "Hello, world!  $id "
+    }*/
+
+    /*@GetMapping()
+    fun home(@PathParam("id") id:Int ): String {
+        return "Hello, world! $id"
+    }*/
+
+@GetMapping()
+fun home(@RequestBody model:Model ): String {
+    return "hola profe Marco, este es mi deber !  ${model.id} ${model.name} ${model.age}"
+}
+}
